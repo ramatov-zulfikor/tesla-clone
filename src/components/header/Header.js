cb22d4de5e0f88372c2e4c5d0676eabcef482ede
ramatov-zulfikor <ramatovv@gmail.com>
2021-01-16 13:53:19 +0500

    header + assets

---
+++ src/components/header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './header.css'
+
+import TeslaLogo from '../../assets/teslaLogoSmall.svg'
+
+export default function Header() {
+   return (
+      <div className="header">
+
+         <div className="header__logo">
+            <img src={TeslaLogo} alt="Tesla Logo" />
+         </div>
+
+         <div className="header__center">
+            <p>Model S</p>
+            <p>Model 3</p>
+            <p>Model X</p>
+            <p>Model Y</p>
+            <p>Solar Roof</p>
+            <p>Solar Panel</p>
+         </div>
+
+         <div className="header__right">
+            <p>Shop</p>
+            <p>Tesla Account</p>
+         </div>
+         
+      </div>
+   )
+}
